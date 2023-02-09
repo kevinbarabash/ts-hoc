2b451e938080e8dbee9837c8e1dcfa404c1ea587
Kevin Barabash <kevinb@khanacademy.org>
2023-02-09 17:03:00 -0500

    formatting + default props

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,23 +1,42 @@
 import * as React from "react";
 
-import {withFoo, withFooBar} from "./index";
-import type {WithFooProps} from "./index";
+import { withFoo, withFooBar } from "./index";
+import type { WithFooProps } from "./index";
 
-type Props = WithFooProps & {name: string, greeting: string};
+type Props = WithFooProps & { name: string; greeting?: string };
 
-const MyComponent: React.FC<Props> = (props) => {
-    const {greeting, name} = props;
-    return <h1>{greeting}: {name}</h1>
-}
+const MyComponent: React.FC<Props> = ({ greeting = "world", name}) => {
+  return (
+    <h1>
+      {greeting}: {name}
+    </h1>
+  );
+};
 
 const MyFooBarComponent = withFooBar(MyComponent);
 
 const MyFooComponent = withFoo("baz")(MyComponent);
 
 const MyApp: React.FC<{}> = () => {
-    return <div>
-        <MyComponent foo="" greeting="hello" name="world" />
-        <MyFooBarComponent greeting="hello" name="world" />
-        <MyFooComponent greeting="hello" name="world" />
-    </div>;
-}
+  return (
+    <div>
+      <MyComponent foo="" greeting="hello" name="world" />
+      <MyComponent foo="" name="world" />
+      <MyFooBarComponent greeting="hello" name="world" />
+      <MyFooBarComponent name="world" />
+      <MyFooComponent greeting="hello" name="world" />
+      <MyFooComponent name="world" />
+    </div>
+  );
+};
+
+// NOTE: This pattern of defining components doesn't work with HOCs.
+const MyOtherComponent = ({ greeting = "world", name}: Props): React.ReactNode => {
+  return (
+    <h1>
+      {greeting}: {name}
+    </h1>
+  );
+};
+
+const MyOtherFooBarComponent = withFooBar(MyOtherComponent);
